5d829a6a062882e6e23e0a843d4f9574ffbf8272
Hoda Salah <37372736+hodasalah@users.noreply.github.com>
2024-01-20 02:57:40 +0300

    add chalk and try it in the console

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { func1, func2 } = require('./utils');
+const chalk = require('chalk');
 
 const fs = require('fs');
 fs.writeFileSync('text.txt', 'hello this file is created by nodejs!.');
@@ -8,3 +9,44 @@ fs.appendFileSync(
 );
 func1();
 func2();
+const log = console.log;
+/**
+ * *********************************************************
+ * try chalk to color consol lo
+ *
+ *  */
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(
+	chalk.green(
+		'I am a green line ' +
+			chalk.blue.underline.bold('with a blue substring') +
+			' that becomes green again!',
+	),
+);
+
+// ES2015 template literal
+log(`
+        CPU: ${chalk.red('90%')}
+        RAM: ${chalk.green('40%')}
+        DISK: ${chalk.yellow('70%')}
+        `);
+
+// Use RGB colors in terminal emulators that support it.
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
+/***
+ * end chalk colors
+ * *********************************************************************
+ */
